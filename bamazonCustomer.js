aa949d8371ef2c84d0617fecdf5c207e00d6f773
Michael Pesce <Michael Pesce>
2017-06-17 18:56:19 -0400

    added function to prompt user at start

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require('mySQL');
+var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,4 +13,22 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    start();
 });
+
+function start() {
+	inquirer.prompt ({
+		name: "userType",
+		type: "rawlist"
+		message: "Are you here to SHOP? or to WORK?"
+		choices: ["SHOP", "WORK"]
+	}).then(function(anwser){
+	// based on their answer, either call the shop or the work functions
+      if (answer.userType.toUpperCase() === "SHOP") {
+        shopperStart();
+      }
+      else {
+        managerStart();
+      }
+	})
+}
